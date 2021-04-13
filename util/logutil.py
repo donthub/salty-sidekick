acdb9620a7c5c63ecd0816b29015c30284353c33
Beni B <dontsmail@gmail.com>
2021-04-13 22:47:06 +0200

    Added automatic log directory creation, minor refactors.

--- util/logutil.py
+++ util/logutil.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 from logging import Formatter
 from logging import StreamHandler
@@ -9,10 +10,23 @@ class LogUtil:
 
     def initialize(self):
         log_formatter = Formatter('%(asctime)s %(message)s')
-        file_handler = RotatingFileHandler(filename='logs/app.log', maxBytes=1000000000, backupCount=10, encoding='utf-8')
-        file_handler.setFormatter(log_formatter)
-        console_handler = StreamHandler(sys.stdout)
-        console_handler.setFormatter(log_formatter)
+        file_handler = self.init_file_handler(log_formatter)
+        console_handler = self.init_console_handler(log_formatter)
+        self.init_root_logger(file_handler, console_handler)
+
+    def init_file_handler(self, log_formatter):
+        log_filename = "logs/output.log"
+        os.makedirs(os.path.dirname(log_filename), exist_ok=True)
+        handler = RotatingFileHandler(filename=log_filename, maxBytes=1000000000, backupCount=10, encoding='utf-8')
+        handler.setFormatter(log_formatter)
+        return handler
+
+    def init_console_handler(self, log_formatter):
+        handler = StreamHandler(sys.stdout)
+        handler.setFormatter(log_formatter)
+        return handler
+
+    def init_root_logger(self, file_handler, console_handler):
         root_logger = logging.getLogger()
         root_logger.setLevel(logging.INFO)
         root_logger.addHandler(file_handler)
